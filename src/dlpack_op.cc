e54d65b60ef7a8d01d51afdc9b8f2818ad73185d
Minjie Wang <wmjlyjemaine@gmail.com>
2019-11-05 18:57:01 -0500

    git submodule; code style; cuda build

--- src/dlpack_op.cc
+++ src/dlpack_op.cc
@@ -1,24 +1,24 @@
-#include "tensorflow/core/framework/op.h"
-#include "tensorflow/core/framework/shape_inference.h"
+#include <tensorflow/core/framework/op.h>
+#include <tensorflow/core/framework/shape_inference.h>
 
 using namespace tensorflow;
 
 // Stupid Tensorflow will turn the camelcase name into snakecase name
 // To make the name be to_dlpack, the OP is regestered as ToDlpack
 REGISTER_OP("ToDlpack")
-    .Attr("T: {float, double, int32, int64, uint32, uint64}")
-    .Input("in : T")
-    .Output("out : uint64");
+  .Attr("T: {float, double, int32, int64, uint32, uint64}")
+  .Input("in : T")
+  .Output("out : uint64");
 
 // Return a tensor with shape {3}. First is device type, and second is device id.
 // Third is the corresponding value of TF_DTYPE
 // Based on DLPack Protocal
 REGISTER_OP("GetDeviceAndDtype")
-    .Input("in : uint64")
-    .Output("out : int32");
+  .Input("in : uint64")
+  .Output("out : int32");
 
 REGISTER_OP("FromDlpack")
-    .Attr("T: {float, double, int32, int64, uint32, uint64}")
-    .Input("in : uint64")
-    .Output("out : T");
-    // .Output("out : float");
+  .Attr("T: {float, double, int32, int64, uint32, uint64}")
+  .Input("in : uint64")
+  .Output("out : T");
+  // .Output("out : float");
