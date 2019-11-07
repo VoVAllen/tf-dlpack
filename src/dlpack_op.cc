ffcd7a563218fd16c402063db3727e581566c0dd
VoVAllen <jz1749@nyu.edu>
2019-11-07 12:13:04 +0000

    more types

--- src/dlpack_op.cc
+++ src/dlpack_op.cc
@@ -5,12 +5,12 @@ using namespace tensorflow;
 // Stupid Tensorflow will turn the camelcase name into snakecase name
 // To make the name be to_dlpack, the OP is regestered as ToDlpack
 REGISTER_OP("ToDlpack")
-  .Attr("T: {float, double, int32, int64, uint32, uint64}")
+  .Attr("T: {half, float, double, int8, int16, int32, int64, uint32, uint64}")
   .Input("in : T")
   .Output("out : uint64");
 
 REGISTER_OP("FromDlpack")
-  .Attr("T: {float, double, int32, int64, uint32, uint64}")
+  .Attr("T: {half, float, double, int8, int16, int32, int64, uint32, uint64}")
   .Input("in : uint64")
   .Output("out : T");
 
