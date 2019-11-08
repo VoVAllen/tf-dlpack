b4baf88cb590cb978407fdecc73ecee2c3329770
VoVAllen <jz1749@nyu.edu>
2019-11-08 08:14:03 +0000

    fix docker

--- docker/install/ubuntu_install_build.sh
+++
@@ -1,8 +0,0 @@
-# install cmake 3.9
-wget https://cmake.org/files/v3.9/cmake-3.9.0.tar.gz
-tar xvf cmake-3.9.0.tar.gz
-cd cmake-3.9.0
-./configure
-make -j4
-make install
-cd ..
--- docker/install/ubuntu_install_core.sh
+++ docker/install/ubuntu_install_core.sh
@@ -1,4 +1,4 @@
 # install libraries for building c++ core on ubuntu
 apt update && apt install -y --no-install-recommends --force-yes \
         apt-utils git build-essential make wget unzip sudo \
-        libz-dev libxml2-dev ca-certificates
+        libz-dev libxml2-dev ca-certificates cmake
