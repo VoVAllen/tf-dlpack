c660b60e0d046cb7280d960d95cd0d62b120f614
Minjie Wang <wmjlyjemaine@gmail.com>
2020-03-29 16:59:52 +0000

    add docker pull

--- tests/scripts/build_release.sh
+++ tests/scripts/build_release.sh
@@ -6,9 +6,13 @@ BRANCH=$1
 
 rm -rf *.whl
 
+docker pull dgllib/tfdlpack-ci-cpu:03302020
+
 docker run -it --rm -v "$PWD":/workspace \
   --name tfdlpack-build-cpu dgllib/tfdlpack-ci-cpu:latest \
   bash /workspace/build_in_docker.sh $BRANCH OFF
+
+docker pull dgllib/tfdlpack-ci-gpu:03302020
 
 docker run -it --rm -v "$PWD":/workspace \
   --runtime nvidia \
